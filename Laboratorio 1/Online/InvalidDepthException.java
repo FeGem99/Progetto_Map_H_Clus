ffee89b1ec42dbcc069388babab6ab7f3c0b22ad
FeGem99 <f.gemma@studenti.uniba.it>
2024-10-09 17:08:30 +0200

    Lab 1 TERMINATO
    Il laboratorio funziona ed esce l'output richiesto dalla prof

---
+++ Laboratorio 1/Online/InvalidDepthException.java
@@ -0,0 +1,20 @@
+
+
+
+
+@SuppressWarnings("serial")
+/**
+ * Eccezione controllata che indica un errore nella profondità del dendrogramma.
+ * Viene lanciata se la profondità specificata è maggiore del numero di esempi nel dataset.
+ */
+public class InvalidDepthException extends Exception {
+
+    /**
+     * Costruttore che accetta un messaggio di errore.
+     *
+     * @param message Il messaggio di errore da visualizzare.
+     */
+    public InvalidDepthException(String message) {
+        super(message);
+    }
+}
